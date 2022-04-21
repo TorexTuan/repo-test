8d0f68b88b5c9f50a5d61f0a1ec1083d1a6e0929
TorexTuan <kanetuan4444@gmail.com>
2022-04-21 21:51:35 +0700

    add all

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <form action="">
+        <InputField title="Email" placeholder="Enter your email"/>
+        <InputField title="Password" placeholder="Enter your password"/>
+    </form>
+</template>
+
+<script lang="ts">
+import InputField from '../components/InputField.vue'
+
+export default ({
+  components: {
+    InputField
+  }
+})
+</script>
